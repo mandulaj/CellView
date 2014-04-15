82c81893d815479a8800feb1eb671775f5763b4d
Jakub Mandula <jakub.aludnam@gmail.com>
2014-04-15 07:15:29 +0200

    add basic structure

--- script.js
+++ script.js
@@ -10,3 +10,36 @@ ctx.fillStyle = "#FF0000";
 ctx.fillRect(0, 0, 450, 450);
 ctx.fillStyle = "#00FF00";
 ctx.fillRect(450, 450, 450, 450);
+
+
+function CellView(canvas)
+{
+    this.canvas = document.getElementById(canvas);
+    this.dots = [];
+}
+
+CellView.prototype.loadDots = function()
+{
+    
+}
+
+CellView.prototype.getMousePos = function(evt)
+{
+    var rect = this.canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+function Dot(x,y,img)
+{
+    this.img = new Image();
+    this.x = x;
+    this.y = y;
+}
+
+var cell = new CellView("cell");
+cell.canvas.addEventListener("mousemove", function(evt) {
+    //console.log(cell.getMousePos(evt))
+})
