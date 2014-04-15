aeaa29399fbee4d84631ff4d47f547415692df82
Jakub Mandula <jakub.aludnam@gmail.com>
2014-04-15 10:11:43 +0200

    Add Dots to list

--- script.js
+++ script.js
@@ -2,25 +2,18 @@
 var c = document.getElementById("cell");
 var ctx = c.getContext("2d");
 
-ctx.fillStyle = "#0000FF";
-ctx.fillRect(450, 0, 450, 450);
-ctx.fillStyle = "#000000";
-ctx.fillRect(0, 450, 450, 450);
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0, 0, 450, 450);
-ctx.fillStyle = "#00FF00";
-ctx.fillRect(450, 450, 450, 450);
-
-
 function CellView(canvas)
 {
     this.canvas = document.getElementById(canvas);
     this.dots = [];
 }
 
-CellView.prototype.loadDots = function()
+CellView.prototype.loadDots = function(array)
 {
-    
+	for (var i = 0; i < array.length; i++ )
+	{
+		this.dots.push(new Dot(array[i][1], array[i][2], array[i][0]))
+	}
 }
 
 CellView.prototype.getMousePos = function(evt)
@@ -35,11 +28,30 @@ CellView.prototype.getMousePos = function(evt)
 function Dot(x,y,img)
 {
     this.img = new Image();
+    this.imgName = this.makeImgName(img);
     this.x = x;
     this.y = y;
 }
 
+Dot.prototype.makeImgName = function(imgNum)
+{
+	var path = "data/w/";
+	var prfix = "w_";
+	var sufix = ".png"
+	var num = "";
+	
+	for ( var i = 0; i< (3-imgNum.length); i++ )
+	{
+		num += "0"
+	}
+	num += imgNum;
+	
+	return path + prfix + num + sufix;
+}
+
 var cell = new CellView("cell");
+cell.loadDots(Cell_data)
+
 cell.canvas.addEventListener("mousemove", function(evt) {
     //console.log(cell.getMousePos(evt))
 })
